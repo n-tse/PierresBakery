754bf4859aeb8fcc53a7adacfc416ab1b4145c51
Nick Tse <nicholashtse@gmail.com>
2022-07-09 14:10:06 -0700

    incorporate Bread class into Main method

---
+++ Bakery/Models/Bread.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Bakery.Models
+{
+  public class Bread
+  {
+    public int QuantityOrdered { get; set; }
+
+    public Bread (int quantityOrdered)
+    {
+      QuantityOrdered = quantityOrdered;
+    }
+
+    public int CalculateTotalCost (int quantityOrdered)
+    {
+      if (quantityOrdered >= 2)
+      {
+        if (quantityOrdered % 3 == 1)
+        {
+          return quantityOrdered / 3 * 10 + 5;
+        }
+        else
+        {
+          return quantityOrdered / 3 * 10;
+        }
+      }
+      else
+      {
+        return 5;
+      }
+    }
+  }
+}
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
+using Bakery.Models;
 
-namespace Bakery
+namespace Store
 {
   public class Program
   {
     public static void Main()
     {
+      Bread breadOrder = new Bread(0);
       Console.WriteLine("Welcome to Pierre's Bakery!");
       Console.WriteLine("");
       Console.WriteLine("MENU");
@@ -18,10 +20,11 @@ namespace Bakery
       Console.WriteLine("----------------");
       Console.WriteLine("Pastries");
       Console.WriteLine("Buy one for $2 or three for $5");
-      OrderInquiry();
+      OrderInquiry(breadOrder);
+      Console.WriteLine("Thanks for visiting Pierre's. The total for your order is $" + breadOrder.CalculateTotalCost(breadOrder.QuantityOrdered));
     }
 
-    static void IsOrderCorrect(string userResponse)
+    static void IsOrderCorrect(string userResponse, Bread breadOrder)
     {
       if (userResponse.ToLower() == "y")
       {
@@ -29,29 +32,29 @@ namespace Bakery
       } 
       else if (userResponse.ToLower() == "n")
       {
-        OrderInquiry();
+        OrderInquiry(breadOrder);
       }
       else
       {
         Console.WriteLine("Please confirm your order by entering Y/N");
         string yesOrNo = Console.ReadLine();
-        IsOrderCorrect(yesOrNo);
+        IsOrderCorrect(yesOrNo, breadOrder);
       }
     }
 
-    static string OrderInquiry()
+    static void OrderInquiry(Bread breadOrder)
     {
       Console.WriteLine("================");
       Console.WriteLine("How many loaves of bread would you like to buy?");
       string stringLoaves = Console.ReadLine();
       int numberOfLoaves = int.Parse(stringLoaves);
+      breadOrder.QuantityOrdered = numberOfLoaves;
       Console.WriteLine("How many pastries would you like to buy?");
       string stringPastries = Console.ReadLine();
       int numberOfPastries = int.Parse(stringPastries);
       Console.WriteLine("You would like to buy " + numberOfLoaves + " loaves of bread and " + numberOfPastries + " pastries. Is that correct? Y/N"); 
       string yesOrNo = Console.ReadLine();
-      IsOrderCorrect(yesOrNo);
-      return yesOrNo;
+      IsOrderCorrect(yesOrNo, breadOrder);
     }
   }
 }
