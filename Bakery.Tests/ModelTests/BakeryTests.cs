1463afac0904a89348cbcb5568a795aa84fa7eb1
Nick Tse <nicholashtse@gmail.com>
2022-07-09 14:30:20 -0700

    Combine tests into a single BakeryTests file; run initial 'failed' test for Pastry class; test successfully failed

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -4,34 +4,41 @@ using Bakery.Models;
 namespace Bakery.Test
 {
   [TestClass]
-  public class BreadTests
+  public class BakeryTests
   {
     [TestMethod]
     public void CalculateTotalCost_Return5_5()
     {
-      Bread testBread = new Bread(0, 0);
+      Bread testBread = new Bread(0);
       Assert.AreEqual(5, testBread.CalculateTotalCost(1));
     }
 
     [TestMethod]
     public void CalculateTotalCost_CorrectlyAddTotalCost_15()
     {
-      Bread testBread = new Bread(0, 0);
+      Bread testBread = new Bread(0);
       Assert.AreEqual(15, testBread.CalculateTotalCost(4));
     }
 
     [TestMethod]
     public void CalculateTotalCost_CorrectlyAddTotalCost_10()
     {
-      Bread testBread = new Bread(0, 0);
+      Bread testBread = new Bread(0);
       Assert.AreEqual(10, testBread.CalculateTotalCost(3));
     }
 
     [TestMethod]
     public void CalculateTotalCost_CorrectlyAddTotalCost_30()
     {
-      Bread testBread = new Bread(0, 0);
+      Bread testBread = new Bread(0);
       Assert.AreEqual(30, testBread.CalculateTotalCost(9));
+    }
+
+    [TestMethod]
+    public void CalculateTotalCost_Return2_2()
+    {
+      Pastry testPastry = new Pastry(0);
+      Assert.AreEqual(2, testPastry.CalculateTotalCost(1));
     }
   }
 }
