8ff0576d26d95685352947c383d2285b70664a6d
Nick Tse <nicholashtse@gmail.com>
2022-07-09 11:50:16 -0700

    add method to handle if user does not appropriately confirm their order

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -13,12 +13,12 @@ namespace Bakery
       Console.WriteLine("================");
       Console.WriteLine("Bread");
       Console.WriteLine("SPECIAL PROMOTION!! Buy 2, get 1 free!");
-      Console.WriteLine("   One loaf: $5");
-      Console.WriteLine("   Two or three loaves: $10");
+      Console.WriteLine(" One loaf: $5");
+      Console.WriteLine(" Two or three loaves: $10");
       Console.WriteLine("----------------");
       Console.WriteLine("Pastries");
       Console.WriteLine("Buy one for $2 or three for $5");
-      IsOrderCorrect(OrderInquiry());
+      OrderInquiry();
     }
 
     static void IsOrderCorrect(string userResponse)
@@ -50,6 +50,7 @@ namespace Bakery
       int numberOfPastries = int.Parse(stringPastries);
       Console.WriteLine("You would like to buy " + numberOfLoaves + " loaves of bread and " + numberOfPastries + " pastries. Is that correct? Y/N"); 
       string yesOrNo = Console.ReadLine();
+      IsOrderCorrect(yesOrNo);
       return yesOrNo;
     }
   }
