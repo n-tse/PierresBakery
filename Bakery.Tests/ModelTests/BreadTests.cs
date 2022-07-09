ab736c3576932ab50223af68264ad8d5503ea2b2
Nick Tse <nicholashtse@gmail.com>
2022-07-09 12:48:24 -0700

    Write test for if order quantity is 1; test passes

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -12,5 +12,12 @@ namespace Bakery.Test
       Bread testBread = new Bread(0, 0);
       Assert.AreEqual(1, testBread.CalculateTotalCost(5));
     }
+
+    [TestMethod]
+    public void CalculateTotalCost_Return5_5()
+    {
+      Bread testBread = new Bread(0, 0);
+      Assert.AreEqual(5, testBread.CalculateTotalCost(1));
+    }
   }
 }
