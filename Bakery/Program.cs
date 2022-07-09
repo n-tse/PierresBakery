56668860e79059766a1dc3f5e35140abd05a00d9
Nick Tse <nicholashtse@gmail.com>
2022-07-08 19:09:39 -0700

    Add main method with menu and prices/offers

---
+++ Bakery/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("");
+      Console.WriteLine("MENU");
+      Console.WriteLine("================");
+      Console.WriteLine("Bread");
+      Console.WriteLine("SPECIAL PROMOTION!! Buy 2, get 1 free!");
+      Console.WriteLine("   One loaf: $5");
+      Console.WriteLine("   Two or three loaves: $10");
+      Console.WriteLine("----------------");
+      Console.WriteLine("Pastries");
+      Console.WriteLine("Buy one for $2 or three for $5");
+      Console.WriteLine("================");
+      Console.WriteLine("How many would you like to buy");
+
+    }
+  }
+}
