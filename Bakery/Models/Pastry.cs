b1779655aa2f21996d4f4c8de35dc0d33547c608
Nick Tse <nicholashtse@gmail.com>
2022-07-09 14:33:39 -0700

    update Pasty class' CalculateTotalCost method to handle 1 pastry; test passes

---
+++ Bakery/Models/Pastry.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Bakery.Models
+{
+  public class Pastry
+  {
+    public int QuantityOrdered { get; set; }
+
+    public Pastry (int quantityOrdered)
+    {
+      QuantityOrdered = quantityOrdered;
+    }
+
+    public int CalculateTotalCost (int quantityOrdered)
+    {
+      if (quantityOrdered > 2)
+      {
+        return 0;
+      }
+      else
+      {
+        return 2;
+      }
+    }
+  }
+}
